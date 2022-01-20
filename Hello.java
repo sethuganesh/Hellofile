c34aeba31d9e2076827bf43a8d6a7867df85d250
sethuganesh <98066921+sethuganesh@users.noreply.github.com>
2022-01-19 20:24:52 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/****************************************************************
+* JRE Lab1
+*****************************************************************
+* Sethupathy Ganesan
+* 01/19/2022
+* CMSC 255 C90
+****************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Sethupathy Ganesan");
+		System.out.println("JRE Lab1");
+		System.out.println("Install the software and run a Introductory/warmup code");
+		System.out.println("01/19/2022");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+		
+	}
+}
